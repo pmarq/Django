52ca74f98cf96930746399ab918693de9c561eb9
pmarq <pedro.hmo91@gmail.com>
2018-07-03 13:14:08 -0300

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -1,53 +1,110 @@
+#<Login>
+
 Cliente
-  idCliente
-  nome
-  sobrenome
-  cpf
+  idCliente PK, AI, NN
+  ClienteNome
+  ClienteSobrenome
+  ClienteCpf
+  ClienteLogin
+  ClienteSenha
+  
+#CadastroCliente
+#idCliente:1 
+#ClienteNome:PEDRO
+#ClienteSobrenome:MARQUES
+#ClienteCpf:06493600600 
+
   
   
+   
   
  Cliente_Endereco
+    idCliente_Endereco
     idCliente
-    cep
-    rua
-    numero
-    complemento
-    bairro
-    cidade
-    estado
+    Cliente_EnderecoCep
+    Cliente_EnderecoRua
+    Cliente_EnderecoNumero
+    Cliente_EnderecoComplemento
+    Cliente_EnderecoBairro
+    Cliente_EnderecoCidade
+    Cliente_EnderecoEstado
+
+#CADASTRO ENDEREÇO
+
+#idCliente_Endereco: 1
+#idCliente: 1
+#Cliente_EnderecoCep:31910110    
+#Cliente_EnderecoRua: RUA ARAUA
+#Cliente_EnderecoNumero: 145
+#Cliente_EnderecoComplemento : CASA/FUNDOS
+#Cliente_EnderecoBairro: SAO PAULO
+#Cliente_EnderecoCidade: BELO HORIZONTE
+#Cliente_EnderecoEstado: MG    
+ 
     
 Cliente_Contato
-    idCliente
-    telefone
-    whatsapp
-    email
+  idCliente_Contato
+  idCliente
+  Cliente_ContatoTelefone
+  Cliente_ContatoWhatsapp
+  Cliente_ContatoEmail
+  
+#idCliente_Contato: 1
+#idCliente: 1
+#Cliente_ContatoTelefone : 34325941
+#Cliente_ContatoWhatsapp: 31984404639
+#Cliente_ContatoEmail: pedro.hmo91@gmail.com
 
+  
+  
 Carro
-  modelo = varchar
-  motor = varchar
-  ano = int
-  versao = varchar
-  km = int
-  combustivel =  ['Alcool','Gasolina','Flex']
-  cor = varchar
-  portas =  ['2 portas','4 portas']
+  idCarro
+  CarroModelo = varchar
+  CarroMotor = varchar
+  CarroAno = int
+  CarroVersao = varchar
+  CarroKm = int
+  CarroCombustivel =  ['Alcool','Gasolina','Flex']
+  CarroCor = varchar
+  CarroPortas =  ['2 portas','4 portas']
 
+#idCarro: 1
+#CarroModelo: Celta
+#CarroMotor: 1.0 8v VHC
+#CarroAno: 2004
+#CarroVersao: SUPER
+#CarroKm: 138.000
+#CarroCombustivel: ['Gasolina']
+#CarroCor: ['PRATA']
+#CarroPortas:  ['2 portas']
+
+  
 Cliente_Carro1:
+  idCliente_Carro1
   idCarro
   idCliente
+
+#idCliente_Carro1: 1
+#idCarro: 1
+#idCliente: 1
+
   
-Cliente_Carro2:
-  idCarro
-  idCliente  
   
-CarroOleoMotor
+#</Login>  
+
+
+
+
+
+OleoMotor
   idOleoMotor
   idCarro
   OleoMotorProduto = FKidProduto ['oleo']
-  capacidade = varchar
-  bujao = FKidProduto ['Bujao']
-  filtroOleo = FKidProduto ['Filtro de Oleo']
+  OleoMotorCapacidade = varchar
+  OleoMotorBujao = FKidProduto ['Bujao']
+  OleoMotorFiltroOleo = FKidProduto ['Filtro de Oleo']
  
+
 CarroArrefecimento
   idArrefecimento
   idCarro
@@ -57,14 +114,15 @@ CarroArrefecimento
 
 CarroTabelaFipe
   idCarro
-  preco
+  CarroTabelaFipePreco
     
   
-CarroDefeito 
+CarroDefeito
+  idCarroDefeito
   idCarro
   idServico
   idProduto
-  DefeitoTitulo
+  CarroDefeitoTitulo
   
 #Exemplo de um defeito:
 #Carro: Fox 1.0 8v Flex
