e7019a37a96e1b6777b2e95ddb788db84f237baf
pmarq <pedro.hmo91@gmail.com>
2018-07-08 19:03:44 -0300

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -1,197 +1,92 @@
-#<Login>
-
-Cliente
-  idCliente PK, AI, NN
-  ClienteNome
-  ClienteSobrenome
-  ClienteCpf
-  ClienteLogin
-  ClienteSenha
-  
-#CadastroCliente
-#idCliente:1 
-#ClienteNome:PEDRO
-#ClienteSobrenome:MARQUES
-#ClienteCpf:06493600600 
-
-  
-  
-   
-  
- Cliente_Endereco
-    idCliente_Endereco
-    idCliente
-    Cliente_EnderecoCep
-    Cliente_EnderecoRua
-    Cliente_EnderecoNumero
-    Cliente_EnderecoComplemento
-    Cliente_EnderecoBairro
-    Cliente_EnderecoCidade
-    Cliente_EnderecoEstado
-
-#CADASTRO ENDEREÇO
-
-#idCliente_Endereco: 1
-#idCliente: 1
-#Cliente_EnderecoCep:31910110    
-#Cliente_EnderecoRua: RUA ARAUA
-#Cliente_EnderecoNumero: 145
-#Cliente_EnderecoComplemento : CASA/FUNDOS
-#Cliente_EnderecoBairro: SAO PAULO
-#Cliente_EnderecoCidade: BELO HORIZONTE
-#Cliente_EnderecoEstado: MG    
- 
-    
-Cliente_Contato
-  idCliente_Contato
-  idCliente
-  Cliente_ContatoTelefone
-  Cliente_ContatoWhatsapp
-  Cliente_ContatoEmail
-  
-#idCliente_Contato: 1
-#idCliente: 1
-#Cliente_ContatoTelefone : 34325941
-#Cliente_ContatoWhatsapp: 31984404639
-#Cliente_ContatoEmail: pedro.hmo91@gmail.com
-
-  
-  
-Carro
-  idCarro
-  CarroModelo = varchar
-  CarroMotor = varchar
-  CarroAno = int
-  CarroVersao = varchar
-  CarroKm = int
-  CarroCombustivel =  ['Alcool','Gasolina','Flex']
-  CarroCor = varchar
-  CarroPortas =  ['2 portas','4 portas']
-
-#idCarro: 1
-#CarroModelo: Celta
-#CarroMotor: 1.0 8v VHC
-#CarroAno: 2004
-#CarroVersao: SUPER
-#CarroKm: 138.000
-#CarroCombustivel: ['Gasolina']
-#CarroCor: ['PRATA']
-#CarroPortas:  ['2 portas']
-
-  
-Cliente_Carro1:
-  idCliente_Carro1
-  idCarro
-  idCliente
-
-#idCliente_Carro1: 1
-#idCarro: 1
-#idCliente: 1
-
-  
-  
-#</Login>  
 
 
+class Montadora(models.Model):
+    nome = models.CharField(max_length=80)
+    def __str__(self):
+        return self.nome
+
+class Carro(models.Model):
+    modelo = models.CharField(max_length=120)
+    motor = models.CharField(max_length=50)
+    ano = models.IntegerField
+    montadora = models.ForeignKey(Montadora, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.modelo + " " + self.motor + " " + self.ano
+    pass
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=80)
+    email = models.EmailField
+    telefone = models.CharField(max_length=15)
+    carro = models.ForeignKey(Carro, on_delete=models.CASCADE)
+    data = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.nome + " " + self.carro
+    pass
 
 
+class Fabricante(models.Model):
+    nome = models.CharField(max_length=80)
 
-OleoMotor
-  idOleoMotor
-  idCarro
-  OleoMotorProduto = FKidProduto ['oleo']
-  OleoMotorCapacidade = varchar
-  OleoMotorBujao = FKidProduto ['Bujao']
-  OleoMotorFiltroOleo = FKidProduto ['Filtro de Oleo']
- 
+    def __str__(self):
+        return self.nome
+    pass
 
-CarroArrefecimento
-  idArrefecimento
-  idCarro
-  capacidadeSistema = varchar
-  
-  
 
-CarroTabelaFipe
-  idCarro
-  CarroTabelaFipePreco
-    
-  
-CarroDefeito
-  idCarroDefeito
-  idCarro
-  idServico
-  idProduto
-  CarroDefeitoTitulo
-  
-#Exemplo de um defeito:
-#Carro: Fox 1.0 8v Flex
-#Defeito: Vazamento de oleo no motor
+class Peca(models.Model):
+    descricao = models.CharField(max_length=120)
+    codigo = models.CharField(max_length=30)
+    fabricante = models.OneToOneField(Fabricante, on_delete=models.CASCADE)
+    Preco = models.DecimalField(max_digits=6, decimal_places=2)
 
-#Produtos a serem conferidos:
-  #Tampa de valvula
-    #ServiçoTitulo: Troca do sistema     
-  
-  #Interruptor de pressão do Oleo
-  
-  #AntiChama
-  
-    #ServiçoTitulo: Troca do sistema de anti-chamas
-    #ServicoProdutos: ['AntiChama-CWB1536']
-    #ServicoMenorPreco:[60,00]
-    #ServicoMaiorPreco: [90,00]
-  
-  #Carter
+    def __str__(self):
+        return self.descricao + " " + self.codigo + " - " +self.fabricante
+    pass
 
-#Serviço:
-#
-#
-  
-    
-    
-Produto
-  idProduto
-  ProdutoDescricao
-  ProdutoCodigo
-  ProdutoPreco
-  ProdutoEstoque
-  ProdutoAplicacao
-  
 
-Servico
-  idServico
-  idCarro
-  ServicoTitulo
-  ServicoPreco
-  ServicoTempoExecucao
-  idProduto=Manytomany
-  
-  
-//////////////////////////////////////////////////////  
+class Pivo(models.Model):
 
-ClienteOficina
-  idCliente
-  idOficina
-  
-Oficina
-  idClienteOficina
-  OficinaNome
-  OficinaTelefone
-  OficinaRating
+    peca = models.ForeignKey(Peca, on_delete=models.CASCADE)
+    lado = models.CharField(max_length=20)
 
-  
-Anuncio
-  idCliente
-  idCarro
-Detalhes
-  Carro.Ano
-  Carro.Km
-  Carro.combustivel
-  AceitoTroca =True or False
-  Carro.cor
-  Carro.porta
-    
-Acessorios
-Observacoes
-  
-  
+
+class TerminalDirecao(models.Model):
+    peca = models.ForeignKey(Peca, on_delete=models.CASCADE)
+    lado = models.CharField(max_length=20)
+
+class ArticulacaoAxial(models.Model):
+    peca = models.ForeignKey(Peca, on_delete=models.CASCADE)
+    lado = models.CharField(max_length=20)
+
+class AmortDt(models.Model):
+    peca = models.ForeignKey(Peca, on_delete=models.CASCADE)
+    lado = models.CharField(max_length=20)
+
+class AmortTras(models.Model):
+    peca = models.ForeignKey(Peca, on_delete=models.CASCADE)
+    lado = models.CharField(max_length=20)
+
+class kitAmortDt(models.Model):
+    peca = models.ForeignKey(Peca, on_delete=models.CASCADE)
+    lado = models.CharField(max_length=20)
+
+class kitAmortTras(models.Model):
+    peca = models.ForeignKey(Peca, on_delete=models.CASCADE)
+    lado = models.CharField(max_length=20)
+
+class SuspensaoCarro(models.Model):
+
+    car = models.ForeignKey(Carro, on_delete=models.CASCADE)
+    pivo = models.ForeignKey(Pivo, on_delete=models.CASCADE)
+    terminal = models.ForeignKey(TerminalDirecao, on_delete=models.CASCADE)
+    axial = models.ForeignKey(ArticulacaoAxial, on_delete=models.CASCADE)
+    amortecedor_dt = models.ForeignKey(AmortDt, on_delete=models.CASCADE)
+    amortecedor_tras = models.ForeignKey(AmortTras, on_delete=models.CASCADE)
+    kit_amort_dt = models.ForeignKey(kitAmortDt, on_delete=models.CASCADE)
+    kit_amort_tras = models.ForeignKey(kitAmortTras, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.car + " Peças para suspensão ."
+
